23bcf38489defec381715e528eb0907cd604bf1c
gupei <gupei515@gmail.com>
2022-03-13 22:59:50 -0400

    adding webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 module.exports = (env, args) => {
     console.log(`Building for env='${process.env.NODE_ENV} with mode='${args.mode}''`);
+
+    const productionMode = args.mode === "production";
+
     const commonConfig = {
         context: __dirname,
         entry: {
             main: "./src/index.tsx",
-            output: {
-                path: path.join(__dirname, "dist/"),
-                publicPath: "/",
-                filename: "[name].[contenthash].js",
-            },
-            resolve: {
-                extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".scss", ".css"],
-                alias: aliases
-            }
+        },
+        output: {
+            path: path.join(__dirname, "dist/"),
+            publicPath: "/",
+            filename: "[name].[contenthash].js",
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".scss", ".css"],
+            alias: aliases
         }
     }
 }
