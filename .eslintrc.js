6de39f6dceebbcba05336e6f0e3c1fa1bb21f020
gupei <gupei515@gmail.com>
2022-03-13 22:11:02 -0400

    adding webpack.config.js

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    root: true,
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+        "plugin:jest/recommended"
+    ],
+    plugin: [
+        "react-hooks",
+        "@typescript-eslint"
+    ],
+    env: {
+        es6: true,
+        browser: true,
+        jest: true
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2018,
+        ecmaFeature: {
+            jsx: true
+        },
+        project: "./src/tsconfig.json",
+        sourceType: "module"
+    },
+    ignorePatterns: ["node_modules", "*.d.ts", "*.js"],
+    rules: {
+        "@typescript-eslint/array-type": ["error", { "array-simple": true }],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-empty-interface": ["error", { "allowSingleExtends": true }],
+        "@typescript-eslint/no-require-imports": "error",
+        "@typescript-eslint/no-throw-literal": "error",
+        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+        "@typescript-eslint/no-explicit-any": "off",
+        "jest/expect-expect": "off",
+        "prettier-prettier": "error",
+        "react/props-types": "off",
+        "react/display-name": "off",
+        "react/jsx-curly-brace-presence": "error",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "react/react-in-jsx-scope": "off"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = (env, args) => {
+    console.log(`Building for env='${process.env.NODE_ENV} with mode='${args.mode}''`);
+    const commonConfig = {
+        context: __dirname,
+        entry: {
+            main: "./src/index.tsx",
+            output: {
+                path: path.join(__dirname, "dist/"),
+                publicPath: "/",
+                filename: "[name].[contenthash].js",
+            },
+            resolve: {
+                extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".scss", ".css"],
+                alias: aliases
+            }
+        }
+    }
+}
