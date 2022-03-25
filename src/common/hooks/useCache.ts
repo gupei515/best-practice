0621d1bdf00731cbfcab3bbdb3a29639ec15241d
gupei <gupei515@gmail.com>
2022-03-25 00:03:15 -0400

    add cache hook

--- src/common/hooks/useCache.ts
+++ src/common/hooks/useCache.ts
@@ -1,12 +1,13 @@
-export const cache =  <T extends unknown>(promiseFunc: () => Promise<T>) => {
+export const cache = <T>(promiseFunc: () => Promise<T>) => {
     let result: T;
     let reject: any;
     let promiseCalledOnce = 0;
     const f = () => {
-        if(promiseCalledOnce === 1)
-        return result && Promise.resolve(result) || reject && Promise.reject(reject);
+        if (promiseCalledOnce === 1) return (result && Promise.resolve(result)) || (reject && Promise.reject(reject));
         promiseCalledOnce = 1;
-        return  promiseFunc().then(rlt => result = rlt).catch(err => reject = err);
-    }
+        return promiseFunc()
+            .then((rlt) => (result = rlt))
+            .catch((err) => (reject = err));
+    };
     return f;
-}
+};
