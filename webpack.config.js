0e7e6a55293cf0b81241a9704be9ad8eb5f9f49e
gupei <gupei515@gmail.com>
2022-04-12 01:26:35 -0400

    adding webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("lodash");
+const { IgnorePlugin, EnvironmentPlugin } = require("webpack");
 const webpack = require("webpack");
 
 module.exports = (env, args) => {
@@ -17,7 +21,95 @@ module.exports = (env, args) => {
         },
         resolve: {
             extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".scss", ".css"],
-            alias: alias
+            alias: aliases,
         },
-    }
-}
+        module: {
+            rules: [
+                {
+                    test: "/.tsx?$/",
+                    loader: "ts-loader",
+                    exclude: /node_modules/,
+                    options: {
+                        transpileOnly: true, // type checking in ForkIsCheckerWebpackPlugin
+                    },
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|woff|woff2|eot|ttf|svg)$/,
+                    loader: "url-loader",
+                    option: {
+                        esModule: false,
+                        publicPath: "/",
+                        limit: 10 * 1024,
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new ForkTsCheckerWebpackPlugin({ typescript: { configFile: "./src/tsconfig.json" } }),
+            new IgnorePlugin({
+                resourceRegExp: /^\.\/locale$/,
+                contextRegExp: /moment$/,
+            }),
+            new HtmlWebpackPlugin({
+                title: "CRM 360",
+                filename: "index.html",
+                template: "./src/index.ejs",
+                chunks: ["main"],
+                inject: true,
+                hash: true,
+                meta: productionMode
+                    ? {}
+                    : {
+                          region: { name: "accrm:region", content: "NA" },
+                      },
+            }),
+            new HtmlWebpackPlugin({
+                title: "MyQrCode",
+                filename: "my-qr-code.html",
+                template: "./src/index.ejs",
+                chunks: ["myQrCode"],
+                inject: true,
+                meta: productionMode
+                    ? {}
+                    : {
+                          region: { name: "accrm:region", content: "NA" },
+                      },
+            }),
+            new CopyPlugin({
+                patterns: [
+                    { from: "assets/favicons", to: "favicons" },
+                    { from: "assets/images", to: "images" },
+                    { from: "assets/events-images", to: "events-images" },
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash.css]",
+                chunkFilename: "[name].[contenthash].css",
+            }),
+            new EnvironmentPlugin({ PROJECT_VERSION: "loal", ...env }),
+        ],
+        stats: {
+            errors: true,
+            errorDetails: true,
+            moduleTrace: true,
+        },
+        optimization: {
+            emitOnErrors: false,
+            moduleIds: "deterministic",
+            runtimeChunk: "single",
+            splitChunks: {
+                chunks: "all",
+            },
+        },
+        performance: {
+            hints: false,
+        },
+    };
+
+    if (testEnv) return merge(cypressConfig, commonConfig, devConfig);
+    return productionMode ? merge(commonConfig, prodConfig) : merge(commonConfig, devConfig);
+};
