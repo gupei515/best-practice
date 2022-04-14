b828274f9740b1fbf07529e4bf35e5f540ba1b8b
gupei <gupei515@gmail.com>
2022-04-13 20:12:15 -0400

    add webpack dev

---
+++ webpack.dev.config.js
@@ -0,0 +1,23 @@
+const PBA = "E152626";
+
+module.exports = {
+    mode: "development",
+    devtool: "eval-cheap-module-source-map",
+    cache: true,
+    devServer: {
+        port: 9000,
+        hot: true,
+        historyApiFallback: true，
+        devMiddleware: {stats: "minimal"},
+        proxy: ["/api", "/actuator"].reduce((acc, curr) => ({
+            ...acc,
+            [curr]: {
+                target: "https://localhost:8000",
+                secure: false,
+                onProxyReq: function(proxyReq) {
+                    proxyReq.setHeader("Cookie", `IMPERSONATED_USER=${PBA}; CAN_IMPESONATE=yes`)
+                }
+            }
+        }), {})
+    }
+}
