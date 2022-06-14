23f9543af3a51beda42eb9601ed92c10d53abe4f
gupei <gupei515@gmail.com>
2022-06-14 16:44:20 -0400

    throttle set

--- src/DebounceThrottle/DebounceThrottle.tsx
+++ src/DebounceThrottle/DebounceThrottle.tsx
@@ -4,21 +4,50 @@ import { Input } from "reactstrap";
 export const DebounceThrottle = () => {
     const [defaultValue, setDefaultValue] = useState("");
     const [debounceValue, setDebounceValue] = useState("");
+    const [throttleValue, setThrottleValue] = useState("");
 
     // 等input结束了以后一下子，想象一下点菜的过程
     const debounce = (cb, delay = 5000) => {
         let timeout;
         return (...args) => {
             clearTimeout(timeout);
-            timeout = setTimeout(() => cb(...args), delay);
+            timeout = setTimeout(() => cb(...args), delay); // setTimeout call for once
+        };
+    };
+
+    const throttle = (cb, delay = 5000) => {
+        let shouldWait = false;
+        let waitingArgs;
+        const timeoutFunc = () => {
+            if (waitingArgs === null) {
+                shouldWait = false;
+            } else {
+                cb(...waitingArgs);
+                waitingArgs = null;
+                setTimeout(timeoutFunc, delay);
+            }
+        };
+
+        return (...args) => {
+            if (shouldWait) {
+                waitingArgs = args;
+                return;
+            }
+
+            cb(...args);
+            shouldWait = true;
+
+            setTimeout(timeoutFunc, delay);
         };
     };
 
     const updateDebounceText = debounce((text) => setDebounceValue(text));
+    const updateThrottleText = throttle((text) => setThrottleValue(text));
 
     const onChange = (e) => {
         setDefaultValue(e.target.value);
         updateDebounceText(e.target.value);
+        updateThrottleText(e.target.value);
     };
 
     return (
@@ -32,10 +61,10 @@ export const DebounceThrottle = () => {
                 <span>Debounce:&nbsp;</span>
                 <span>{debounceValue}</span>
             </div>
-            {/* <div>
+            <div>
                 <span>Throttle:</span>
-                <span>{throttle}</span>
-            </div> */}
+                <span>{throttleValue}</span>
+            </div>
         </div>
     );
 };
